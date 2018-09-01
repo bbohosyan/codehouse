dd3f45f7bbf6f4ba63986d1230ee8e351a8fec11
siqqQ <b.bohosyan@gmail.com>
2018-09-02 00:36:15 +0300

    Added UserController and getAll and createUser method

---
+++ src/main/java/com/codehouse/codehouse/UserController.java
@@ -0,0 +1,25 @@
+package com.codehouse.codehouse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    public UserRepository some;
+    @PostMapping("create")
+    public User createUser(){
+        User user = new User("Gosho");
+        some.save(user);
+        return user;
+    }
+
+    @GetMapping("/getAll")
+    public List<User> getAll(){
+        return some.findAll();
+    }
+}
