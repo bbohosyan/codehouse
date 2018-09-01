3a98626a1b62567ff3b1e3677f3eae2918df864c
siqqQ <b.bohosyan@gmail.com>
2018-09-02 00:26:16 +0300

    Initial commit

---
+++ src/main/java/com/codehouse/codehouse/CodehouseApplication.java
@@ -0,0 +1,12 @@
+package com.codehouse.codehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodehouseApplication.class, args);
+    }
+}
