a99688364821db8cdbf08f5b557d59794cce1f3c
siqqQ <b.bohosyan@gmail.com>
2018-09-11 10:21:51 +0300

    Added restrictions to what requests can unauthorised user make

--- src/main/java/com/codehouse/codehouse/UserController.java
+++ src/main/java/com/codehouse/codehouse/UserController.java
@@ -1,6 +1,8 @@
 package com.codehouse.codehouse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +20,18 @@ public class UserController {
         return user;
     }
 
-    @GetMapping("/getAll")
+    @GetMapping("getAll")
     public List<User> getAll(){
         return some.findAll();
+    }
+
+    @PostMapping("logout")
+    public void logout() throws Exception {
+
+    }
+
+    @GetMapping("/test")
+    public String someString(){
+        return "31321321";
     }
 }
